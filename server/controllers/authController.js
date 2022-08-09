6c6e8a5f1dd2f9ed6aebd94bc0e3379e9a16809b
adarsh50 <adarshsulegai50@gmail.com>
2022-08-09 23:03:46 +0530

    setup login flow

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,37 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const login = (req, res) => {};
+const createToken = (_id, refresh_token = true) => {
+  const expireIn = refresh_token ? '1d' : '3d';
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: expireIn });
+};
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   try {
-    const user = User.signup(email, password);
-    console.log(user);
-  } catch (err) {
-    console.log(error);
+    const user = await User.signup(email, password);
+    const token = createToken(user._id, true);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
+};
 
-  res.status(200).json({});
+const login = async (req, res) => {
+  const cookies = req.cookies;
+  console.log('req cook', cookies?.jwt);
+  const { email, password } = req.body;
+  console.log(email, password);
+  try {
+    const user = await User.login(email, password);
+    const accessToken = createToken(user._id);
+    const refreshToken = createToken(user._id, false);
+    res.cookie('jwt', refreshToken, { httpOnly: true, expireIn: '3d' });
+    res.status(200).json({ email, accessToken });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const logout = (req, res) => {};
